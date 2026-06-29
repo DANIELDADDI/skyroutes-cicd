05ce1f2560bb036a4c9c35664241654ae561e8e8
daniel <tirukelem21@gmail.com>
2026-06-29 21:25:26 +0300

    add dependency search pagee

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,9 @@ export default function App() {
      
       return (
             <>
-                  {/* <FlightSearch/> */}
-                  <FlightList/>
-                  <FlightListPreview/>
+                  <FlightSearch/>
+                  {/* <FlightList/>
+                  <FlightListPreview/> */}
             </>
       );
 }
