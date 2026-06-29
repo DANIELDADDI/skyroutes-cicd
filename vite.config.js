d4b56fb8be4bbc27f31723c8b88c21ac15d63c49
daniel <tirukelem21@gmail.com>
2026-06-29 22:06:47 +0300

    solving gh pages -d from build to dist

--- vite.config.js
+++ vite.config.js
@@ -4,17 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
       plugins: [react()],
-      base:"/skyroutes-cicd/",
-      server:{
-            open: true,                 // Automatically open the app in the browser on server start
-            port: 3001,                // default value is 5173
-            // strictPort: true,         // Set to true to exit if port is already in use, instead of automatically trying the next available port.  
-            // host: localhost,         // default value , Set this to 0.0.0.0 or true to listen on all addresses, including LAN and public addresses.
-            
-            // allowedHosts:['localhost','.localhost','shegertravel.et'] ,       // default []
-            // https: https.ServerOptions,
-
-
-
+      base: "/skyroutes-cicd/",
+      server: {
+            open: true,            
+            port: 3001,            
       }
 })
